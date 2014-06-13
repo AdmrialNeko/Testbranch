ee3e2d0ce0f940a97d83b089e985ec5dd894500e
Prelang Builder <builder@prelang.com>
2014-06-13 23:57:59 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gatewaygamer::Application.config.secret_key_base = '56139f7e63ecaf49ba862d3923e2646611e2cd505bc2d509786f89863f39520f144aee808fedc847a6d8ada3ed302ad7a6139b3d35fff36ae20fc0748dc5f97c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gatewaygamer::Application.config.session_store :cookie_store, key: '_gatewaygamer_session'
